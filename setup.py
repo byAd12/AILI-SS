d45ed1a047e7477f103f1a62835d629a1da6fa27
byAd12 <92215714+byAd12@users.noreply.github.com>
2025-07-23 16:10:54 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from cx_Freeze import setup, Executable
-
-ejecutable = Executable(script="app.pyw", base=None)
-setup(name="Multi_App", version="1.0", description="...", author="byAd12",
-    options={"build_exe": {"include_files": ["func/"]}},
-    executables=[ejecutable]
-)
